523c3a172860dc3c863098cc1daaa43dec476ea7
T V Bhaskarachary <tvbchary@gmail.com>
2022-12-18 07:19:17 +0800

    update

--- Basics/server.js
+++ Basics/server.js
@@ -2,7 +2,7 @@
 // 1. Node runs on a server - not in a browser (backend not frontend)
 // 2. The console is the terminal window
     console.log('Hello World'); // run this using the command node <filename> *extension no bar : on the terminal
-const { log } = require('console');
+
 // 3. Global object instead of window object
 //  console.log(global);
 // 4. Has Common Core modules 
@@ -11,22 +11,24 @@ const { log } = require('console');
 
 const os = require('os');
 const path = require('path');
-//const math = require('./math');
+//const math = require('./math'); // Second method to import other files
 const {add, subtract, multiply, divide} = require('./math');
 
 console.log(add(6,9));
 console.log(multiply(6,9));
 console.log(subtract(6,9));
 console.log(divide(6,9));
-// console.log(os.type());
-// console.log(os.version());
-// console.log(os.homedir());
 
-// console.log(__dirname);
-// console.log(__filename);
+//OS and File names fetching operations
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
 
-// console.log(path.dirname(__filename));
-// console.log(path.basename(__filename));
-// console.log(path.extname(__filename));
+console.log(__dirname);
+console.log(__filename);
 
-// console.log(path.parse(__filename));
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+
+console.log(path.parse(__filename));
