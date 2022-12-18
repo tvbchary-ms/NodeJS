440054f121a90b76a81a8a50cdae6c5c2dcf18c1
T V Bhaskarachary <tvbchary@gmail.com>
2022-12-19 02:39:46 +0800

    update 3

---
+++ nodemon/README.md
@@ -0,0 +1,5 @@
+#### This folder contains information about different npm packages such as:
+
+* nodemon - Monitors for the change in a given file - When changes it runs the file again
+* date-fns - Date and Time functions
+* uuid - Creates a unique id each time
---
+++ nodemon/index.js
@@ -0,0 +1,8 @@
+const {format} = require('date-fns'); //Date time package
+const {v4: uuid} = require('uuid'); //{v4:uuid} stands for import v4 as uuid
+
+
+console.log(format(new Date(), 'yyyy-MM-dd\tHH:mm:ss'));
+
+console.log(uuid()); // creates a different id each time we run
+console.log();
